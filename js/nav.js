6fe0bf51bac2595a85dc91a0ac360e939e21abe4
yzbbanban <yzbbanban@live.com>
2017-10-20 17:00:25 +0800

    上传dfm

---
+++ js/nav.js
@@ -0,0 +1,107 @@
+/**
+ * 功能列表
+ */
+var navs = 
+[
+    {
+        "title": "监控中心",
+//      "icon": "fa-cubes",
+        "spread": true,
+        "children": [
+            {
+                "title": "地图监控",
+//              "icon": "&#xe62b;",
+                "href": "mapMonitoring.html"
+            },
+            {
+                "title": "轨迹回放",
+//              "icon": "&#xe63c;",
+                "href": "replayingOrbit.html"
+            },
+            {
+                "title": "电子围栏",
+//              "icon": "&#xe63c;",
+                "href": "electronicFence.html"
+            }
+        ]
+    },
+    {
+        "title": "报警设置",
+//      "icon": "fa-cogs",
+        "spread": false,
+        "children": [
+            {
+                "title": "电子围栏报警设置",
+//              "icon": "fa-table",
+                "href": "electronicFenceAlarm.html"
+            },
+            {
+                "title": "基础报警设置",
+//              "icon": "fa-navicon",
+                "href": "baseAlarm.html"
+            },
+             {
+                "title": "报警统计",
+//              "icon": "fa-navicon",
+                "href": "alarmStatistics.html"
+            }
+        ]
+    },
+    {
+        "title": "报表中心",
+//      "icon": "&#x1002;",
+        "spread": false,
+        "children": [
+            {
+                "title": "行驶统计",
+//              "icon": "fa-check-square-o",
+                "href": "drivingStatistics.html"
+            },
+            {
+                "title": "停车报表",
+//              "icon": "fa-table",
+                "href": "stopReport.html"
+            },
+            {
+                "title": "故障报表",
+//              "icon": "fa-navicon",
+                "href": "troubleReport.html"
+            },
+             {
+                "title": "车身数据报表",
+//              "icon": "fa-navicon",
+                "href": "carReport.html"
+            }
+        ]
+    },
+    {
+        "title": "系统设置",
+//      "icon": "&#x1002;",
+        "spread": false,
+        "children": [
+            {
+                "title": "部门管理",
+//              "icon": "fa-github",
+                "href": "departmentManager.html"
+            },
+            {
+                "title": "子账号管理",
+//              "icon": "fa-qq",
+                "href": "userManager.html"
+            },
+            {
+                "title": "车辆管理",
+//              "icon": "&#xe609;",
+                "href": "carManager.html"
+            }
+        ]
+    },
+//  {
+//      "title": "这是一级导航",
+//      "icon": "fa-stop-circle",
+//      "href": "https://www.baidu.com",
+//      "spread": false
+//  }
+]
+
+;
